58bfa59e391beb503634052acb630ba8c3cc3e9f
Menard, Alexis <alexis.menard@intel.com>
2025-05-04 17:03:31 -0400

    More production deployment fixes

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/will-i-pr',
   build: {
     outDir: 'docs/',
     emptyOutDir: true,
